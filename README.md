31d28bb9827187757797dcb5aa27c3e7c6dfdb50
Jayden <thanhnghiep1007@gmail.com>
2024-05-07 19:35:20 -0700

    Working on unit test

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab 5 - Starter
+
+1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+
+Probably no if it need "full" features I think, because sending message might involve interacting with other components within the application. However, it can be used to test partially like message validation and handling user input correctly.
+
+2. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+
+Yes! We can use unit testing for this because it's a specific functionality within a single unit, and do not require interation with other components.
+
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,98 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('valid phone number 1', () => {
+    expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('valid phone number 2', () => {
+    expect(isPhoneNumber('123-4567890')).toBe(true);
+});
+
+test('invalid phone number 1', () => {
+    expect(isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('invalid phone number 2', () => {
+    expect(isPhoneNumber('123-456-789g')).toBe(false);
+});
+
+
+test('valid email 1', () => {
+    expect(isEmail('abc@gmail.com')).toBe(true);
+});
+
+test('valid email 2', () => {
+    expect(isEmail('abc@ucsd.edu')).toBe(true);
+});
+
+test('invalid email 1', () => {
+    expect(isEmail('abcgmail.com')).toBe(false);
+});
+
+test('invalid email 2', () => {
+    expect(isEmail('abc@ucsd')).toBe(false);
+});
+
+
+test('invalid password 1', () => {
+    const password = 'ab1';
+    expect(isStrongPassword(password)).toBe(false);
+});
+
+test('invalid password 2', () => {
+    const password = '1abcdef';
+    expect(isStrongPassword(password)).toBe(false);
+});
+
+test('valid password 1', () => {
+    const password = 'abcdef1';
+    expect(isStrongPassword(password)).toBe(true);
+});
+
+test('valid password 2', () => {
+    const password = 'a1bcdef';
+    expect(isStrongPassword(password)).toBe(true);
+});
+
+
+test('valid date 1', () => {
+    const date = '1/1/2024';
+    expect(isDate(date)).toBe(true);
+});
+
+test('valid date 2', () => {
+    const date = '01/01/2024';
+    expect(isDate(date)).toBe(true);
+});
+
+test('invalid date 1', () => {
+    const date = '1/1/24';
+    expect(isDate(date)).toBe(false);
+});
+
+test('invalid date 2', () => {
+    const date = '01/01/24';
+    expect(isDate(date)).toBe(false);
+});
+
+
+test('valid hex color 1', () => {
+    const color = '012345';
+    expect(isHexColor(color)).toBe(true);
+});
+
+test('valid hex color 2', () => {
+    const color = '000';
+    expect(isHexColor(color)).toBe(true);
+});
+
+test('invalid hex color 1', () => {
+    const color = '000FFU';
+    expect(isHexColor(color)).toBe(false);
+});
+
+test('invalid hex color 2', () => {
+    const color = 'FFU';
+    expect(isHexColor(color)).toBe(false);
+});
